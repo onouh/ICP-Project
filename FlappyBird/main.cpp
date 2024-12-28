a9a76f898454c12e9c45820a00d6be621e297506
Omar Nouh <onouh7@gmail.com>
2024-12-28 09:32:57 +0200

    Initial Commit

---
+++ FlappyBird/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FlappyBird
+//
+//  Created by Omar Nouh on 12/28/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
